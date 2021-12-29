8bf6e63407fa05669a5d417ad547d9b80409bc2f
Nicolas Salvadore <salvadore.nicolas@gmail.com>
2021-12-29 14:50:38 +0000

    fix cron path and loggging system

--- backup.sh
+++ backup.sh
@@ -3,112 +3,97 @@
 # Description: Backup script for linux container with lxd
 # Forked from https://github.com/triopsi/backup_all_lxc
 #---------------------------------------------------------------------
-APWD=$(pwd);
-
-function errorAndQuit {
-    echo "Exit now!"
+errorAndQuit() {
+    logmsg "Exit now!"
     exit 1
 }
 
-function wait_bar () {
-  for i in {1..10}
-  do
-    printf '= %.0s' {1..$i}
-    sleep $1s
-  done
+logmsg() {
+    echo "$(date +'%F %T') $1"
 }
+
+# the snap repository is not in the default cron $PATH, so we add it here
+export PATH=$PATH:/snap/bin
 
 #Edit the path to the backup directory
 BACKUPDIR="/backup/data"
 
-
-# ------------------------------------------------------------------------------------------
-# Logging
-#-------------------------------------------------------------------------------------------
-#Those lines are for logging purposes
-echo "Welcome to the backup script for lxc"
-echo "========================================="
-echo "Backup started..."
-echo "========================================="
-
-
-BACKUPSTARTDATE=$(date +"%y-%m-%d-%H-%M")
-BACKUPDATE=$(date +"%y-%m-%d")
+BACKUPSTARTDATE=$(date +'%F %T')
+BACKUPDATE=$(date +%F)
 LXC=$(which lxc)
 lxclist=$(lxc list --format csv -c n)
 
 if [ ! -d $BACKUPDIR ];then
     mkdir -p $BACKUPDIR
-    echo "[✓] Backupdir is created"
+    logmsg "[✓] Backupdir is created"
 fi
-echo "[✓] Start backup at $BACKUPSTARTDATE"
+logmsg "[-] Starting backup at $BACKUPSTARTDATE"
 for container in $lxclist
 do
-    echo "------------------------- Begin backup for $container -------------------------"
+    logmsg "Begining backup for $container"
 
     if $LXC info $container > /dev/null 2>&1; then
-        echo "[✓] Container $container found, continuing.."
+        logmsg "[✓] Container $container found, continuing.."
     else
-        echo "[E] Container $container NOT found, exiting lxdbackup"
+        logmsg "[E] Container $container NOT found, exiting lxdbackup"
         continue
     fi
 
     #Create Snapshot
-    echo "[✓] Create snapshot"
+    logmsg "[-] Creating snapshot"
     if $LXC snapshot $container $BACKUPDATE; then
-        echo "[✓] Succesfully created snaphot $BACKUPDATE on container $container"
+        logmsg "[✓] Succesfully created snaphot $BACKUPDATE on container $container"
     else
-        echo "[E] Could not create snaphot $BACKUPDATE on container $container"
+        logmsg "[E] Could not create snaphot $BACKUPDATE on container $container"
         errorAndQuit
     fi
 
     #Publish snapshot
-    echo "[✓] Snapshot pubplish"
-    if $LXC publish --force $container/$BACKUPDATE --alias $container-backup-$BACKUPDATE; then
-        echo "[✓] Succesfully published an image of $container-backup-$BACKUPDATE"
+    logmsg "[-] Publishing snapshot"
+    if $LXC publish --force --quiet $container/$BACKUPDATE --alias $container-backup-$BACKUPDATE; then
+        logmsg "[✓] Succesfully published an image of $container-backup-$BACKUPDATE"
     else
-        echo "[E] Could not publish create image from $container-backup-$BACKUPDATE"
+        logmsg "[E] Could not publish image from $container-backup-$BACKUPDATE"
         errorAndQuit
     fi
 
     #exists backup dir with date as directoryname
-    echo "[✓] Create backup directory"
     if [ ! -d $BACKUPDIR/$BACKUPDATE ];then
         mkdir $BACKUPDIR/$BACKUPDATE
+	logmsg "[✓] Backup directory created"
     fi
     #Export container as tar
-    echo "[✓] Create image, export as tar"
-    if $LXC image export $container-backup-$BACKUPDATE $BACKUPDIR/$BACKUPDATE/$container; then
-        echo "[✓] Succesfully export $container-backup-$BACKUPDATE to $BACKUPDIR/$BACKUPDATE/$container"
-        ls -all $BACKUPDIR/$BACKUPDATE
+    logmsg "[-] Exporting image"
+    if $LXC image export --quiet $container-backup-$BACKUPDATE $BACKUPDIR/$BACKUPDATE/$container; then
+        logmsg "[✓] Succesfully export $container-backup-$BACKUPDATE to $BACKUPDIR/$BACKUPDATE/$container"
     else
-        echo "[E] Could not export $container-backup-$BACKUPDATE from container $container"
+        logmsg "[E] Could not export $container-backup-$BACKUPDATE from container $container"
         errorAndQuit
     fi
 
     #delete image
-    echo "[✓] Clean image"
+    logmsg "[-] Cleaning image"
     if $LXC image delete $container-backup-$BACKUPDATE; then
-        echo "[✓] Succesfully delete temp image for $container"
+        logmsg "[✓] Succesfully delete temp image for $container"
     fi
 
     #delete snapshot
-    echo "[✓] Clean snapshot"
+    logmsg "[-] Cleaning snapshot"
     if $LXC delete $container/$BACKUPDATE; then
-        echo "[✓] Succesfully delete temp snapshot for $container"
+        logmsg "[✓] Succesfully delete temp snapshot for $container"
     fi
 
     #save the config file
-    echo "[✓] Backup config file"
+    logmsg "[-] Backup config file"
     if [ -e /var/lib/lxd/containers/$container/backup.yaml ];then
         cp /var/lib/lxd/containers/$container/backup.yaml $BACKUPDIR/$BACKUPDATE/$container.yaml
-        echo "[✓] Backup config file success"
+        logmsg "[✓] Succesfully copied LXC config file"
     fi
-    echo "------------------------- backup $container end -------------------------"
+    logmsg "Ending backup for $container"
 done
 
-echo "exporting LXD configuration"
+logmsg "exporting LXD configuration"
 lxd init --dump > $BACKUPDIR/$BACKUPDATE/lxd.config
 
-BACKUPENDDATE=$(date +"%y-%m-%d-%H-%M")
-echo "[✓] Backup is done at $BACKUPENDDATE" 
+BACKUPENDDATE=$(date +'%F %T')
+logmsg "[✓] Backup is done at $BACKUPENDDATE" 
